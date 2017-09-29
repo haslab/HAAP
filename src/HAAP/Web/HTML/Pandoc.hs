1bd49a23fce436ede411cb903457aeec52218933
Hugo Pacheco <hpacheco@gmail.com>
2017-09-29 11:14:12 +0100

    latest

--- src/HAAP/Web/HTML/Pandoc.hs
+++ src/HAAP/Web/HTML/Pandoc.hs
@@ -12,11 +12,11 @@ import Debug.Trace
 asPandocHTML :: (Pandoc -> Pandoc) -> String -> String
 asPandocHTML f str = case readHtml def str of
     Left err -> error $ show err
-    Right pandoc -> trace ("pandoc: " ++ show pandoc) $ writeHtmlString def (f pandoc)
+    Right pandoc -> writeHtmlString def (f pandoc)
     
 pandocChangeLinkUrls :: (String -> String) -> Pandoc -> Pandoc
 pandocChangeLinkUrls furl = everywhere (mkT flink)
     where
     flink :: Inline -> Inline
-    flink (Link attrs inlines (url,name)) = {-trace ("changing " ++ show url ++ " to " ++ show (furl url)) $ -} Link attrs inlines (furl url,name)
+    flink (Link attrs inlines (url,name)) = Link attrs inlines (furl url,name)
     flink i = i
