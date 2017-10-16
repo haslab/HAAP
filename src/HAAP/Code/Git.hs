56a418ee8cfcd538a84bd8f25762f7e591cf376d
Hugo Pacheco <hpacheco@gmail.com>
2017-10-16 23:48:20 +0100

    version

---
+++ src/HAAP/Code/Git.hs
@@ -0,0 +1,15 @@
+module HAAP.Code.Git where
+
+import HAAP.IO
+import HAAP.Core
+
+import qualified Data.Text as Text
+
+import Shelly
+
+shGitVersion :: Sh String
+shGitVersion = do
+    res <- shCommand "git" ["log","-n","1"]
+    let out = Text.unpack $ resStdout res
+    let v = last $ words $ head $ lines out
+    return v
